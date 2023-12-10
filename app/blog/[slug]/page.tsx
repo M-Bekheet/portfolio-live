c40298f1fd98edf6d33fbbdefd321cd8865db674
Mahmoud Bekheet <mahmoud.a.bekheet@gmail.com>
2023-12-11 00:02:07 +0200

    make all slugs lowercase

--- app/blog/[slug]/page.tsx
+++ app/blog/[slug]/page.tsx
@@ -20,7 +20,7 @@ export async function generateMetadata(
   { params, searchParams }: Props,
   parent: ResolvingMetadata
 ): Promise<Metadata> {
-  const slug = params.slug;
+  const slug = params.slug?.toLocaleLowerCase();
   const title = (await fetchPostTitleBySlug(slug))[0].title || "";
   return {
     title,
