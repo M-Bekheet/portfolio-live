cc768c5d7280ff7b47e253281f89b041a1e61697
Mahmoud Bekheet <mahmoud.a.bekheet@gmail.com>
2025-06-04 12:37:37 +0300

    update profile info

--- app/utils/constants/profile.ts
+++ app/utils/constants/profile.ts
@@ -1,9 +1,9 @@
 export const profile = {
   title: `Mahmoud Bekheet`,
-  description: `Front-end Developer Specializing in React, Node.js, Express, MongoDB, and TypeScript`,
+  description: `Front-end Developer Specializing in React, Next.js, and TypeScript. I also have work experience with Node.js, Express, and MongoDB.`,
   jobInfo: `Currently leading the web engineering team at Cura, working remotely to develop top-tier telemedicine web applications for a company in Saudi Arabia.`,
   positionPreference:
-    "Core Skills: React | Node.js | Express | MongoDB | TypeScript",
+    "Core Skills: React | Next.js | Node.js | Express | MongoDB | TypeScript",
   author: `@b_kheet`,
   linkedin: "https://www.linkedin.com/in/mahmoud-bekheet/",
   keywords: [
